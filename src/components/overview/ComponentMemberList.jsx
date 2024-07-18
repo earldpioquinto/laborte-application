5efd9694a43909425220dec14f622f23c722e4b6
unknown <earldpioquinto@gmail.com>
2024-07-18 17:17:28 +0800

    updated redirect

--- src/components/overview/ComponentMemberList.jsx
+++ src/components/overview/ComponentMemberList.jsx
@@ -7,7 +7,7 @@ import useStore from '../../store/dataStore';
 const handleActionClick = (action, id, deleteMember) => {
   switch (action) {
     case 'view':
-      window.location.href = `http://localhost:3000/members/profile/${id}`;
+      window.location.href = `../profile/${id}`;
       break;
     case 'delete':
       if (window.confirm('Are you sure you want to delete this member?')) {
